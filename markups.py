2b14226f6ae31376801c0dfb18d7ff7b414a82a8
Александр <98910740+finake0@users.noreply.github.com>
2023-03-09 10:09:22 +0200

    Delete markups.py

--- markups.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-
-
-btnUrlChannel = InlineKeyboardButton(text="1.Подписаться👨‍💻", url=""# ссылка на приватный чат)
-btnDoneSub = InlineKeyboardButton(text="Подписался✅", callback_data="subchanneldone")
-
-checkSubMenu = InlineKeyboardMarkup(row_width=1)
-checkSubMenu.insert(btnUrlChannel)
-checkSubMenu.insert(btnDoneSub)
